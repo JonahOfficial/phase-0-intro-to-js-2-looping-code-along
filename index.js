7e26e4c50b32a5f242dde504beda51da6b7e8abf
Jonah Mwangala <jonahxnl@gmail.com>
2024-03-26 23:28:06 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(stringNamesArray, eventName) {
+    let thankYouCards=[]
+    for (let i=0; i<stringNamesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${stringNamesArray[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouCards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"])
+
+function countDown(firstNumber) {
+    while (firstNumber>0) {
+        console.log(firstNumber)
+        firstNumber -=1
+    }
+    console.log(firstNumber)
+}
